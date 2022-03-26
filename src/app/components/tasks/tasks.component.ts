9a5437cc62fe2bc6477c978b52e6d80dbacbf0d6
Jane Sferrazza <sferrazzajane@gmail.com>
2022-03-25 23:12:03 -0400

    Able to delete the taks

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import {Task} from '../../Task';
-import {TASKS} from '../../mock-tasks';
+
+import { TaskService } from 'src/app/services/task.service';
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task [] = TASKS;
-  constructor() { }
+  tasks: Task [] = [];
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+     this.taskService.getTasks().subscribe((tasks)=> this.tasks = tasks);
+  }
+  deleteTask(task: Task){
+    this.taskService.deleteTask(task).subscribe(
+    ()=> (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+    );
+
   }
 
 }
