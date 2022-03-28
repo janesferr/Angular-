a59379d4c5b287d9179b40750da8172c7caaf2cb
Jane Sferrazza <sferrazzajane@gmail.com>
2022-03-27 23:01:16 -0400

    Containers added in css

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -3,7 +3,11 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {Task} from '../Task';
 
-
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
 
 @Injectable({
   providedIn: 'root'
@@ -18,6 +22,10 @@ export class TaskService {
   deleteTask(task: Task): Observable<Task> {
     const url = `${this.apiUrl}/${task.id}`;
     return this.http.delete<Task>(url);
+  }
+  updateTaskReminder(task: Task): Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions);
   }
   
 }
